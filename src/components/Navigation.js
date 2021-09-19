037a55967f7f569240aac6dfd14226a5d277d08c
Juun Pak <june.mintlatte@gmail.com>
2021-09-19 16:03:09 +0900

    💄 Update changing navigation link color

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,9 +6,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = ({ signedInUser }) => {
+  const resetColor = () => {
+    document
+      .querySelectorAll('.link-item')
+      .forEach(item => (item.style.color = '#999'));
+  };
+
+  const onTargetClick = event => {
+    const parentEl = event.target.closest('.Navigation-link');
+    if (parentEl.id === 'link-home') {
+      resetColor();
+      document
+        .querySelectorAll('#home-item')
+        .forEach(item => (item.style.color = '#1da1f2'));
+    }
+    if (parentEl.id === 'link-profile') {
+      resetColor();
+      document
+        .querySelectorAll('#profile-item')
+        .forEach(item => (item.style.color = '#1da1f2'));
+    }
+  };
+
   return (
     <ul className="Navigation-box">
-      <li className="Navigation-link">
+      <li className="Navigation-link" id="link-home" onClick={onTargetClick}>
         <Link to="/">
           <FontAwesomeIcon
             icon={faHome}
@@ -21,11 +43,18 @@ const Navigation = ({ signedInUser }) => {
           </span>
         </Link>
       </li>
-      <li className="Navigation-link">
+      <li className="Navigation-link" id="link-profile" onClick={onTargetClick}>
         {signedInUser && (
           <Link to="/profile">
-            <FontAwesomeIcon icon={faUser} size="2x" className="link-item" />{' '}
-            <span className="link-item">프로필</span>
+            <FontAwesomeIcon
+              icon={faUser}
+              size="2x"
+              className="link-item"
+              id="profile-item"
+            />{' '}
+            <span className="link-item" id="profile-item">
+              프로필
+            </span>
           </Link>
         )}
       </li>
